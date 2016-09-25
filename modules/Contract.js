cae94a9df953e8589ddb58f4d0f3466b98685bf4
Colton Colcleasure <colshacol@gmail.com>
2016-09-24 23:45:07 -0500

    Styled contract view

--- modules/Contract.js
+++ modules/Contract.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router'
+import Footer from './Footer'
 
 export default class Contract extends Component {
   constructor(props) {
@@ -10,14 +11,14 @@ export default class Contract extends Component {
       <div  className="contract">
         <img src="./assets/contract.svg"></img>
         <h1>Tarzan's Car rental agreement</h1>
-        <ol>
+        <ul>
           <li>Sunday, September 25, 2016</li>
           <li>Pick-up: 1pm</li>
           <li>Drop-off: 3pm</li>
           <li>Return with gas</li>
           <li>No Smoking</li>
           <li>60 mile radius perimeter</li>
-        </ol>
+        </ul>
         <Link to="/approval"><img id="like" src="./assets/like.svg"></img></Link>
         <Footer/>
       </div>
