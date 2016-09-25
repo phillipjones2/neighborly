f46f8f23b03a3efe8d2f146dc50558983f0105da
Phillip Jones <phillipemersonjones@gmail.com>
2016-09-25 01:30:58 -0500

    make the nav neighbor.ly a link

--- modules/Nav.js
+++ modules/Nav.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router'
 import NavLink from "./NavLink"
 
 export default class Nav extends Component {
@@ -9,7 +10,7 @@ export default class Nav extends Component {
     return (
       <div className="nav">
         <img src="./assets/user.svg"></img>
-        <h1>neighbor<span id="ly">.ly</span></h1>
+        <Link to ="/"> <h1>neighbor<span id="ly">.ly</span></h1></Link>
         <img src="./assets/settings.svg"></img>
       </div>
     )
