06bafd5f22d4987791c9a864bfb3002e3b4e9a72
Phillip Jones <phillipemersonjones@gmail.com>
2016-09-25 00:25:28 -0500

    pending approval page

--- modules/Approval.js
+++ modules/Approval.js
@@ -1,14 +1,36 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router'
+import Footer from './Footer'
 
 export default class Approval extends Component {
   constructor(props) {
     super(props)
+    this.state = {
+      image: "./assets/userWait.svg"
+    }
+  }
+  componentDidMount(){
+    setTimeout(function () {
+      this.setState({image:"./assets/userYes.svg"},() => {
+        alert('hahl')
+      })
+    }, 2000);
   }
   render() {
     return (
       <div  className="approval">
-        <h1>APPROVAL</h1>
-        
+        <img src="./assets/hourglass1.svg"></img>
+        <h1>Pending Approval</h1>
+        <div id="userShake">
+          <img className="userApproval" src="./assets/user.svg"></img>
+          <img className="userApproval" src={this.state.image}></img>
+        </div>
+        <div id="nameShake">
+          <p>Jon</p>
+          <p>Tarzan</p>
+        </div>
+        <img id="handShake" src="./assets/agreement.svg"></img>
+        <Footer/>
       </div>
     )
   }
