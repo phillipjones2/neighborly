678050cbad660005c9a6d1263c8437e3e97d95cd
Phillip Jones <phillipemersonjones@gmail.com>
2016-09-25 12:48:23 -0500

    add

--- modules/Logo.js
+++ modules/Logo.js
@@ -6,7 +6,11 @@ export default React.createClass({
     return (
       <div className="logo">
         <h2>Powered by <span className="bold">handshakes</span>.</h2>
-        <img src="./assets/team.svg"></img>
+        {/* <img src="./assets/team.svg"></img> */}
+        <img src="./assets/Data-Combo-Chart-icon.png"></img>
+        <img src="./assets/metrics-icon.png"></img>
+        <img src="./assets/Pie-Graph.png"></img>
+        <img src="./assets/SocialMediaMarketingIcon-10.png"></img>
       </div>
     )
   }
